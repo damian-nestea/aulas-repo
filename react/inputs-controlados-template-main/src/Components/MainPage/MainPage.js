82b82ad335577d3ab380f47ae173dbdca2c86f9c
Damian Martinez <damian@hotmail.cz>
2023-02-09 05:45:58 -0300

    feature resolução de exercícios feitos na aula de inputs controlados

--- react/inputs-controlados-template-main/src/Components/MainPage/MainPage.js
+++ react/inputs-controlados-template-main/src/Components/MainPage/MainPage.js
@@ -1,20 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {MainContainer, Form, Input} from './styles'
 
 function MainPage() {
+
+  const [nome , setNome] = useState("");
+  const [idade , setIdade] = useState("");
+  const [email , setEmail] = useState("");
+  const [senha , setSenha] = useState("");
+
+  const onChangeNome = (event) => {
+    setNome(event.target.value);
+  }
+
+  const onChangeIdade = (event) => {
+    setIdade(event.target.value);
+  }
+
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const onChangeSenha = (event) => {
+    setSenha(event.target.value);
+  }
+
+  const btnEnviar = () =>{
+    alert(`Nome: ${nome}. Idade: ${idade}. E-mail: ${email}. Senha: ${senha}`);
+    setNome("");
+    setIdade("");
+    setEmail("");
+    setSenha("");
+  }
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       <Form>
         <label>
           Nome:
-          <Input />
+          <Input required value = {nome}  onChange = {onChangeNome}/>
         </label>
         <label>
           Idade:
-          <Input/>
+          <Input value = {idade} onChange = {onChangeIdade}/>
+        </label>
+        <label>
+          E-mail:
+          <Input required type = {'email'} placeholder={'Digite o seu email'} value = {email} onChange = {onChangeEmail}/>
+        </label>
+        <label>
+          Senha
+          <Input required type = {'password'} value = {senha} onChange = {onChangeSenha}/>
         </label>  
-        <button>Enviar dados</button>
+        <button type='submit' onClick={btnEnviar}>Enviar dados</button>
       </Form>
     </MainContainer>
   )
